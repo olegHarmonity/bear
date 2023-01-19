cdf04a693c7a9e45066ea890d20150ea08b71b40
oleg <oleg.svinarcuk@harmonity-group.com>
2023-01-19 15:22:48 +0100

    new camera orbit

---
+++ script.js
@@ -0,0 +1,64 @@
+function binarySearch(arr, elem) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  if (arr[middle] === elem) {
+    return middle;
+  }
+  return -1;
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop(){
+    if(!this.first) {
+        return 0
+    } else {
+        if(this.size = 1 ) {
+            this.first = null
+            this.last = null
+            this.size = 0
+        } else {
+            this.first = this.first.next
+            return --this.size
+        }
+    }
+  }
+}
+
+let stack = new Stack();
+stack.push(1);
+
+console.log(stack);
